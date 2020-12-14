24d459bddab64753bedb91aebda1a532e47992e1
shadabshah <shadabshah781@gmail.com>
2020-12-13 22:43:35 -0800

    created dto class

---
+++ src/main/java/com/jwt/api/Dto.java
@@ -0,0 +1,20 @@
+package com.jwt.api;
+
+public class Dto {
+	String nameString;
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	public String getName2() {
+		return name2;
+	}
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+	String name2;
+	
+	
+}
